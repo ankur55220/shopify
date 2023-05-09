3f40d9f4feb398af069428f911bf1eda82e145c2
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-05-09 12:39:48 +0530

    Update backend.js

--- back/front/src/backend.js
+++ back/front/src/backend.js
@@ -1,2 +1,2 @@
-export const api = process.env.NODE_ENV=='production'?"https://shopify-app-web.onrender.com":"http://localhost:5000/app"
+export const api = process.env.NODE_ENV=='production'?"https://shopify-app-web.onrender.com/app":"http://localhost:5000/app"
 

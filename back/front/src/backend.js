0686288a2cc41e6662e2153db7ba6e8f99042fc4
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-05-09 11:46:11 +0530

    Update backend.js

--- back/front/src/backend.js
+++ back/front/src/backend.js
@@ -1,2 +1,2 @@
-export const api = process.env.NODE_ENV=='production'?"https://shoplist2.herokuapp.com/app":"http://localhost:5000/app"
+export const api = process.env.NODE_ENV=='production'?"https://shopify-app-web.onrender.com":"http://localhost:5000/app"
 

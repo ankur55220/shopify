48f7d98a5faf986ce13456d3765404f1638a6300
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-08-22 17:59:57 +0530

    Update backend.js
    changed it to vercel

--- back/front/src/backend.js
+++ back/front/src/backend.js
@@ -1,2 +1,2 @@
-export const api = process.env.NODE_ENV=='production'?"https://shopify-app-web.onrender.com/app":"http://localhost:5000/app"
+export const api = process.env.NODE_ENV=='production'?"https://shopify-ce0965los-ankur55220.vercel.app/app":"http://localhost:5000/app"
 
